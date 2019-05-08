e3e78a3ec784cedecef13ad695dc67d2e6207688
Kateryna Svyrydenko <k.m.sviridenko@gmail.com>
2019-05-08 21:11:54 +0300

    realization function of add/delete comments; validation of form field; max length #name 255 simbols; create template for new comments with <template>

--- mod2/comment_form_hw/app.js
+++ mod2/comment_form_hw/app.js
@@ -17,11 +17,9 @@
   const NAME_TEXT = document.querySelector('#name');
   const NAME_AREA = NAME_TEXT.setAttribute('maxlength', '255');
   const HIDDEN_TEXT = document.querySelectorAll('.checking');
-  /*const COMMENT_LIST = document.querySelector('.comments-list');*/
- /* const COMMENT_LISTITEM = document.querySelector('.comments-list_item');*/
- /* var button = document.querySelector("input[type='button']");*/
 
 // events declaration
+
   document.querySelector("main").addEventListener('click', onClick);
 
 // function declarations
@@ -48,50 +46,50 @@ function onClick(event) {
  * @param {dom element} el - DOM элемент который проверяется
  * @return {boolean} - true если элемент button, false - если нет
  */
-  function isButton(el) {
-      return el.type == 'button';
+function isButton(el) {
+    return el.type == 'button';
   }
 /*Функция, которая запускает функцию валидации формы и если ее результат true -
  выполняется функция добавления нового комментария*/
-  function addComment(){ 
-      if( validation() )
-          addItem();
+function addComment(){ 
+    if( validation() )
+        addItem();
   }
 
 /*Проверка валидации формы, если #name =='' или #comment == '' - false
 
   @return {boolean} 
 */
-  function validation() {
-      var valid = true;
-        if(NAME_TEXT.value == '') {
-            NAME_TEXT.focus();
-            HIDDEN_TEXT[0].style.display = 'block';
-            valid = false;
-          } else{
-            HIDDEN_TEXT[0].style.display = 'none';
-          }
-          if(COMMENT_TEXT.value == '') {
-            COMMENT_TEXT.focus();
-            HIDDEN_TEXT[1].style.display = 'block';
-            valid = false;
-          } else {
-            HIDDEN_TEXT[1].style.display = 'none';
-          }
-          return valid;
-      }
+function validation() {
+    var valid = true;
+      if(NAME_TEXT.value == '') {
+          NAME_TEXT.focus();
+          HIDDEN_TEXT[0].style.display = 'block';
+          valid = false;
+        } else{
+          HIDDEN_TEXT[0].style.display = 'none';
+        }
+        if(COMMENT_TEXT.value == '') {
+          COMMENT_TEXT.focus();
+          HIDDEN_TEXT[1].style.display = 'block';
+          valid = false;
+        } else {
+          HIDDEN_TEXT[1].style.display = 'none';
+        }
+        return valid;
+    }
 
   /* Функция создает template шаблон для добавления нового комметария пользователем в формате
    #name, #comment и COMMENT_TEXT*/
 
-  function addItem() {
-      var templateComment = document.querySelector('#templateComment');
-      var template = templateComment.content.cloneNode(true);
-      template.querySelector('lorem').textContent = COMMENT_TEXT.value;
-      template.querySelector('.comments-list_date').textContent = formatDate(new Date());
-      template.querySelector('.comments-list_name').textContent = NAME_TEXT.value;
-      document.querySelector('.comments-list').appendChild(template);
-  }
+function addItem() {
+    var templateComment = document.querySelector('#templateComment');
+    var template = templateComment.content.cloneNode(true);
+    template.querySelector('lorem').textContent = COMMENT_TEXT.value;
+    template.querySelector('.comments-list_date').textContent = formatDate(new Date());
+    template.querySelector('.comments-list_name').textContent = NAME_TEXT.value;
+    document.querySelector('.comments-list').appendChild(template);
+}
 
   /*
   Функция вывода формата даты публикации комментария
@@ -113,5 +111,5 @@ function formatDate(date) {
     var seconds = date.getSeconds()
 
     return monthNames[monthIndex] + ' ' + day +', ' + year + ' ' + hours +':' + minutes + ':' + seconds;
-  }
+}
 
